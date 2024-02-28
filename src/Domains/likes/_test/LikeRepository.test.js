f332e0f9ac022353b768b90f6eea91bd07f89f0e
rofinp <rofinugraha.dev@gmail.com>
2024-02-28 21:54:53 +0700

    add a like repository interface test

---
+++ src/Domains/likes/_test/LikeRepository.test.js
@@ -0,0 +1,16 @@
+const LikeRepository = require('../LikeRepository');
+
+describe('a LikeRepository interface', () => {
+  it('should throw an error when invoking abstract behaviour', async () => {
+    // Arrange
+    const likeRepository = new LikeRepository();
+
+    // Action & Assert
+    await expect(likeRepository.addLike('', ''))
+      .rejects.toThrow('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.deleteLikeByCommentIdAndOwner({}))
+      .rejects.toThrow('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.getLikeCountByCommentId(''))
+      .rejects.toThrow('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
