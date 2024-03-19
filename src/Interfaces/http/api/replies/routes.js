f7fd442bfdd35e78105b2c6f1d1554c955df3a0f
rofinp <rofinugraha.dev@gmail.com>
2024-03-19 16:19:42 +0700

    Add API Documentations for replies

--- src/Interfaces/http/api/replies/routes.js
+++ src/Interfaces/http/api/replies/routes.js
@@ -1,18 +1,54 @@
+const Joi = require('joi');
+
 const routes = (handler) => ([
   {
     method: 'POST',
     path: '/threads/{threadId}/comments/{commentId}/replies',
-    handler: (request, h) => handler.postReplyHandler(request, h),
     options: {
+      handler: (request, h) => handler.postReplyHandler(request, h),
       auth: 'forumapi_jwt',
+      plugins: {
+        'hapi-swagger': {
+          description: 'Create a new reply',
+          notes: 'This API endpoint is used to create a new reply for a comment',
+          tags: ['api', 'replies'],
+          validate: {
+            payload: Joi.object({
+              content: Joi.string().required().description('The content of the reply'),
+            }).label('PostRepliesPayload'),
+          },
+          response: {
+            schema: Joi.object({
+              status: Joi.string().valid('success').required(),
+              data: Joi.object({
+                id: Joi.string().required().description('The unique identifier of the reply'),
+                content: Joi.string().required().description('The content of the reply'),
+                owner: Joi.string().required().description('The owner of the reply'),
+              }),
+            }).label('PostRepliesResponse'),
+          },
+        },
+      },
     },
   },
   {
     method: 'DELETE',
     path: '/threads/{threadId}/comments/{commentId}/replies/{replyId}',
-    handler: (request) => handler.deleteReplyHandler(request),
     options: {
+      handler: (request) => handler.deleteReplyHandler(request),
       auth: 'forumapi_jwt',
+      plugins: {
+        'hapi-swagger': {
+          description: 'Soft delete a reply',
+          notes: 'This API endpoint is used to soft delete a reply',
+          tags: ['api', 'replies'],
+          response: {
+            schema: Joi.object({
+              status: Joi.string().valid('success').required(),
+            }).label('DeleteRepliesResponse'),
+          },
+        },
+      },
     },
   },
 ]);
