dd1f8ab25c625e2a2732a4630a3dc748e3b7f648
rofinp <rofinugraha.dev@gmail.com>
2024-03-19 16:29:23 +0700

    Add API Documentations for likes

--- src/Interfaces/http/api/likes/routes.js
+++ src/Interfaces/http/api/likes/routes.js
@@ -1,26 +1,64 @@
+const Joi = require('joi');
+
 const routes = (handler) => ([
   {
     method: 'PUT',
     path: '/threads/{threadId}/likes',
-    handler: (request, h) => handler.putThreadLikeHandler(request, h),
     options: {
+      handler: (request, h) => handler.putThreadLikeHandler(request, h),
       auth: 'forumapi_jwt',
+      plugins: {
+        'hapi-swagger': {
+          description: 'Add or delete a like for a thread',
+          notes: 'This API endpoint is used to add or delete a like for a thread.',
+          tags: ['api', 'likes', 'threads'],
+          response: {
+            schema: Joi.object({
+              status: Joi.string().valid('success').required(),
+            }).label('PutThreadLikesResponse'),
+          },
+        },
+      },
     },
   },
   {
     method: 'PUT',
     path: '/threads/{threadId}/comments/{commentId}/likes',
-    handler: (request, h) => handler.putCommentLikeHandler(request, h),
     options: {
+      handler: (request, h) => handler.putCommentLikeHandler(request, h),
       auth: 'forumapi_jwt',
+      plugins: {
+        'hapi-swagger': {
+          description: 'Add or delete a like for a comment',
+          notes: 'This API endpoint is used to add or delete a like for a comment.',
+          tags: ['api', 'likes', 'comments'],
+          response: {
+            schema: Joi.object({
+              status: Joi.string().valid('success').required(),
+            }).label('PutCommentLikesResponse'),
+          },
+        },
+      },
     },
   },
   {
     method: 'PUT',
     path: '/threads/{threadId}/comments/{commentId}/replies/{replyId}/likes',
-    handler: (request, h) => handler.putReplyLikeHandler(request, h),
     options: {
+      handler: (request, h) => handler.putReplyLikeHandler(request, h),
       auth: 'forumapi_jwt',
+      plugins: {
+        'hapi-swagger': {
+          description: 'Add or delete a like for a reply',
+          notes: 'This API endpoint is used to add or delete a like for a reply.',
+          tags: ['api', 'likes', 'replies'],
+          response: {
+            schema: Joi.object({
+              status: Joi.string().valid('success').required(),
+            }).label('PutReplyLikesResponse'),
+          },
+        },
+      },
     },
   },
 ]);
